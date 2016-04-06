eb6568d64299780fc7fa7b1c5b238916682107a1
DM Kishi <kishi@cantedmedia.com>
2016-04-06 09:36:42 -0700

    Make into WebSocket benchmark

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
 const HTTP_PORT  = 8000;
-const RELAY_PORT = 9999; // To pd, Max/MSP, etc.
-const RELAY_NAME = 'pd'
 
 var server  = require('http').createServer();
 var express = require('express');
 var app     = express();
 var wss     = new (require('ws').Server)({ server: server });
-var net     = require('net');
 var colors  = require('colors');
 var ip      = require('ip');
 
-
-// Connect to relay application
-console.log(`Connecting to ${RELAY_NAME} at port ${RELAY_PORT}...`);
-var relay = net.connect(RELAY_PORT)
-  .on('connect', () => {
-    console.log(`${'SUCCESS!'.green} Relaying device motion data to `,
-                `${RELAY_NAME}.`);
-  })
-  .on('error', () => {
-    console.error(`${'ERROR'.red}: Failed to connect to ${RELAY_NAME}. Ensure`,
-                  `the application is open with a patch listening to port`,
-                  `${RELAY_PORT}.`);
-    process.exit();
-  })
-  .on('close', () => {
-    console.error(`${'ERROR'.red}: Disconnected from ${RELAY_NAME}.`);
-    process.exit();
-  });
 
 
 // Start HTTP server
@@ -43,31 +22,17 @@ server.listen(HTTP_PORT, () => {
 
 
 // Manage WebSockets
-var users = [];
 wss.on('connection', (ws) => {
-  var uid = getUID();
-  users.push(uid);
-  console.log(`${'CONNECT'.green}: User ${uid} connected. Total users:`,
-              `${users.length}`);
+  console.log('WebSocket opened!'.green);
 
-  ws.on('message', (msg) => {
-    // console.log(`User ${uid}: ${msg}`);
-    json = JSON.parse(msg);
-    relay.write(`u${uid} ${json.compass} ${json.roll} ${json.pitch};`);
+  ws.on('message', (raw_msg) => {
+    msg = JSON.parse(raw_msg);
+    if (msg.instruction) {
+      ws.send(String(msg.sig + 1));
+    }
   });
 
   ws.on('close', () => {
-    users.splice(users.indexOf(uid), 1);
-    console.log(`${'DISCONNECT'.red}: User ${uid} disconnected. Total users:`,
-                `${users.length}`);
+    console.log('WebSocket closed!'.red);
   });
 });
- 
-
-// Return smallest integer not in users[] starting from 0.
-function getUID() {
-  users.sort();
-  for (var i = 0; i < users.length; ++i)
-    if (i < users[i]) return i;
-  return i;
-}
--- www/script.js
+++ www/script.js
@@ -1,30 +1,97 @@
-var interval = 15;
-var flash          = document.getElementById('js-flash'),
-    compass_output = document.getElementById('js-compass-output'),
-    roll_output    = document.getElementById('js-roll-output'),
-    pitch_output   = document.getElementById('js-pitch-output'),
-    compass, roll, pitch;
-
-
-function getWebSocketURL() {
-  var loc      = window.location,
-      protocol = (loc.protocol === 'https:') ? 'wss://' : 'ws://';
-  return protocol + loc.host + loc.pathname;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// Get WebSocket URL assuming it's accessed at the root of the same host.
+// E.g. http://localhost:8000               →  ws://localhost:8000
+//      https://www.example.com/index.html  →  wss://www.example.com
+function getWebSocketURL() {
+  return window.location.origin.replace(/^http/i, 'ws');
+}
 
-window.ondeviceorientation = function(evt) {
-  compass = compass_output.innerHTML = evt.alpha;
-  roll    = roll_output.innerHTML    = evt.beta;
-  pitch   = pitch_output.innerHTML   = evt.gamma;
+// Append string "msg" into container element, ex. `newMsg('Hi!');`. Closure
+// structure ensures the container element is sought only once without polluting
+// the global namespace.
+var newMsg = (function() {
+  var container = document.getElementById('js-message-container');
+  return function(msg) {
+    container.innerHTML += msg;
+  };
+})();
+
+
+// 1 ***************************************************************************
+newMsg('<li>Starting benchmark.</li>');
+
+
+// 2 ***************************************************************************
+var url = getWebSocketURL();
+newMsg('<li>Making WebSocket connection at <code>' + url + '</code>....</li>');
+
+
+// 3 ***************************************************************************
+var ws = new WebSocket(url);
+ws.onerror = function(evt) {
+  newMsg('<li>ERROR: Unable to connect!</li>' +
+         '<li>ERROR: Ensure the companion Node.js app is running.</li>' +
+         '<li>ERROR: Aborting benchmark.</li>');
 };
-
-
-flash.innerHTML = 'Establishing WebSocket connection...';
-var ws = new WebSocket(getWebSocketURL());
 ws.onopen = function(evt) {
-  flash.innerHTML = 'Success! WebSocket connection established.'
-  setInterval(function() {
-    ws.send(JSON.stringify({ compass: compass, roll: roll, pitch: pitch }));
-  }, interval);
+  newMsg('<li>SUCCESS!</li>');
+  benchmark1();
 };
+ws.onmessage = function(evt) {
+  console.log(evt);
+};
+
+
+// 4 ***************************************************************************
+function benchmark1() {
+  newMsg('<li>Starting <i>Benchmark 1</i>.</li>');
+  // Instead of sig, just echo back?
+  var instruction = {
+    instruction:       true,
+    sig:               getRandomInt(0, 999999),
+    name:              'Benchmark 1',
+    description:       'sdasdfasdfasdf',
+    time_to_benchmark: 1000,
+    benchmark_dur:     1000,
+    interval:          15,
+    ttl:               3000
+  };
+  ws.onmessage = function(evt) {
+    window.clearTimeout(instruction_timeout);
+
+    if (evt.data !== String(instruction.sig + 1)) {
+      newMsg('<li>ERROR: Bad response from server.</li>' +
+             '<li>ERROR: Aborting benchmark.</li>');
+    }
+
+    var data = makeBenchmark1Data(666);
+    window.setTimeout(function() {
+      var benchmark = window.setInterval(function() {
+        if (!data.length) {
+          window.clearInterval(benchmark);
+        }
+        ws.send(data.pop());
+      }, instruction.interval);
+    }, instruction.time_to_benchmark);
+  };
+  ws.send(JSON.stringify(instruction));
+  var instruction_timeout = window.setTimeout(function() {
+    newMsg('<li>ERROR: Server did not respond to benchmark instruction!</li>' +
+           '<li>ERROR: Aborting benchmark.</li>');
+  }, instruction.ttl);
+}
+
+function makeBenchmark1Data(cnt) {
+  var data = [];
+  for (var i = 0; i < cnt; ++i) {
+    data.push(JSON.stringify({
+      compass: Math.random(),
+      roll:    Math.random(),
+      pitch:   Math.random()
+    }));
+  }
+  return data;
+}
